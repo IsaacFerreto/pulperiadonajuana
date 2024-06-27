325615090c27f53750a16d3f9eaf9eff28664968
IsaacFerreto <168207077+IsaacFerreto@users.noreply.github.com>
2024-06-27 15:28:33 -0600

    LoginyRegistro creados

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useState } from 'react'
+import Registro from './Componentes/Registro'
+import Ingreso from './Componentes/Ingreso'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [pagina,setPagina]=useState(false)
 
+function pageState() {
+  setPagina(!pagina)
+}
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {!pagina?<Ingreso pageState={pageState}/>:<Registro pageState={pageState}/>  }
     </>
+    
   )
 }
 
---
+++ src/Componentes/Registro.jsx
@@ -0,0 +1,116 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import { post, get } from "../hook/useFecht";
+import { validateName,validateEmpty,validateEmail,validatePassword } from "../hook/validateUser";
+import Titulo from "./Titulo";
+const Registro=( {pageState})=>{
+
+    const [movTitulo,setMovTitulo]=useState("")   
+
+const url="http://localhost:3001/users";//URL del API
+
+
+const [usuarios,setUsuarios]=useState([])
+useEffect(()=>{
+    console.log("ENTRA");
+    const getUsuario = async()=>{
+        const  dataUsuarios =await get("users","")
+        setUsuarios(dataUsuarios)
+    }
+    getUsuario()
+    console.log(usuarios);
+},[])
+
+    //Setiando entradas de texto
+const [nombre,setNombre]=useState('');
+const [correo,setCorreo]=useState('');
+const [contrasena,setContrasena]=useState('');
+const [verfContrasena,setVerfContrasena]=useState('');
+
+function verificacion() {
+    //funcion para verificar que las contraseñas coinciden
+if (contrasena===verfContrasena) {
+    //creacion objeto usuario
+    let user={
+    nombre:nombre,
+    correo:correo,
+    contrasena:contrasena
+    }
+    return user
+}else{
+    //cambiar por SWEETALERT
+    alert('No coinciden')
+}
+
+}
+
+
+function usePost() {
+if (validateEmpty(nombre,correo,contrasena)) {
+if (!validateName(usuarios,nombre)) {
+if (validateEmail(correo)) {
+    if (validatePassword(contrasena)) {
+        
+   
+    
+
+
+    const usuario= verificacion();
+post(url,usuario)
+
+alert('usuario creado, sera redirigido a pagina de inicio')
+setTimeout(() => {
+    pageState()
+    
+}, 1000);
+}else{
+    console.log('asdf');
+}
+}else{
+    alert('no funca')
+}
+
+} else{
+    alert('Usuario Existe')
+}
+
+
+}else{
+    alert('LLENE CAMPOS')
+}
+
+    
+}
+
+
+
+
+    return(<>
+     <Titulo titulo={"Linda Hora"} classT={movTitulo}/>
+    <div className="CajaRegistro" 
+    onMouseOver={()=>{setMovTitulo("title-app-hover");}}
+     onMouseOut={()=>{setMovTitulo("")}}>
+        <h1>Registro</h1>
+        <div>
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre" onChange={(e)=>setNombre(e.target.value)}/>
+        </div>
+        <div>
+            <label htmlFor="correo">Correo</label>
+             <input type="text" id="correo" onChange={(e)=>setCorreo(e.target.value)}/>
+        </div>
+        <div>
+            <label htmlFor="contrasena">Contraseña</label>
+             <input type="text" id="contrasena"onChange={(e)=>setContrasena(e.target.value)}/>
+        </div>
+        <div>
+            <label htmlFor="verfContrasena">Repita la Contraseña</label>
+             <input type="text" id="verfContrasena"onChange={(e)=>setVerfContrasena(e.target.value)}/>
+        </div>
+        <button onClick={usePost}>Registro</button>
+        <a onClick={pageState}>Ya tienes cuenta?</a>
+    </div>
+    </>)
+}
+
+export default Registro;
