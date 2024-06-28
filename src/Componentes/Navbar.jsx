99538080163284796d2a629b9972a76ad148f751
IsaacFerreto <168207077+IsaacFerreto@users.noreply.github.com>
2024-06-28 13:37:12 -0600

    ReactRouterAdded

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar=()=>{
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">
-              sobre nosotros
+              Sobre Nosotros
             </a>
           </li>
           <li className="nav-item">
